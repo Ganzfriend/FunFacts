20ba5f737f30e198680ebeff5688135f086c5bad
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-02-24 12:13:35 -0700

    starting to build out trivia component

--- client/src/TriviaSlides.jsx
+++ client/src/TriviaSlides.jsx
@@ -1,6 +1,15 @@
 import React, {useState, useEffect} from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import styles from './styles.js';
+
+const useStyles = makeStyles(styles);
 
 const TriviaSlides = () => {
+  const classes = useStyles();
 
   return (
     <div>
